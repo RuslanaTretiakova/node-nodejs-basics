bf996c1627c3a282b47a5b4549b58c5f9e061043
Ruslana <tretiakovaruslana7@gmail.com>
2025-10-26 21:18:18 +0100

    feat: add solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
+import { fork } from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const filePath = path.join(__dirname, 'files', 'script.js');
+
+  const childProcess = fork(filePath, args.map(String), {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.on('error', (error) => {
+    console.error('Error in child process:', error);
+  });
+
+  childProcess.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['foo', 'bar', 'bla bla', 42]);
+
